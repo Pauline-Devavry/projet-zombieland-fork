0f6ebabe8e19eac11e2075e5a6e2e588d8dffea0
SeurinValentin <valentin.seurin@oclock.school>
2024-09-03 15:38:35 +0200

    S1D2 attraction page added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,11 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./App.css"
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-<<<<<<< HEAD
 import AttractionPage from './pages/AttractionPage.jsx'
-=======
-import Connexion from './pages/Connexion.jsx'
->>>>>>> 40d36496a99a68c641170fa8dddbbd9f3bb07b6c
 
 const router = createBrowserRouter([
   {
@@ -17,18 +13,10 @@ const router = createBrowserRouter([
   {
     path: "hello",
     element: <h1 className='text-red-500'>Hello world page</h1>
-<<<<<<< HEAD
   },
   {
     path: "attractions",
     element: <AttractionPage/>
-=======
-    
-  },
-  { 
-    path: "connexion",
-    element: <Connexion/>
->>>>>>> 40d36496a99a68c641170fa8dddbbd9f3bb07b6c
   }
 ])
 
