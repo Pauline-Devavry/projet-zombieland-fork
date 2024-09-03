4ec256ddf41c909f268a3b6d2ea692426cafea66
MaximeAverty <maxime.averty.pro@gmail.com>
2024-09-03 11:31:30 +0200

    S1D2 Client - Setup the baseLayout of the app

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+
+    return (
+        <header>
+            <h1>my header</h1>
+        </header>
+    )
+
+}
+
+export default Header
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,27 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import "./App.css"
-import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Connexion from './pages/Connexion.jsx'
-
-const router = createBrowserRouter([
-  {
-    index: true,
-    element: <App/>
-  },
-  {
-    path: "hello",
-    element: <h1 className='text-red-500'>Hello world page</h1>
-    
-  },
-  { 
-    path: "connexion",
-    element: <Connexion/>
-  }
-])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-  <RouterProvider router={router} />
+    <App/>
   </StrictMode>,
   )
