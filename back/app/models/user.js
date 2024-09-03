e4ea20a5bc72260a776b371d98741fe1f413e8f2
Jonathanbovy <jonathan.bovy@oclock.school>
2024-09-03 15:10:05 +0200

    S1D2 back - add ticket model

---
+++ back/app/models/user.js
@@ -0,0 +1,37 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../database/client.js";
+
+class User extends Model {}
+
+User.init(
+  {
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    role: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        isIn: [["utilisateur", "administrateur"]],
+      },
+    },
+  },
+  {
+    sequelize: sequelize(),
+    tableName: "user",
+  }
+);
