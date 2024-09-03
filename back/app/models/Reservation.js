bc6fb20cb99f698366fc0f24f9e467d7d887f229
pauline-devavry <pauline.devavry@oclock.school>
2024-09-03 15:37:25 +0200

    S1D2 back - add Reservation model

---
+++ back/app/models/Reservation.js
@@ -0,0 +1,31 @@
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from "../database/client.js";
+
+export class Reservation extends Model {}
+
+Reservation.init({
+    user_id: {
+        type: DataTypes.INTEGER,
+      },
+
+      attraction_id: {
+        type: DataTypes.INTEGER,
+      },
+
+      visit_date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+      },
+      
+      status: {
+        type:DataTypes.STRING(20),
+        allowNull:false,
+        validate: {
+            isIn : [[confirmée, annulée ]]
+        }
+      },
+    }, 
+    {
+      sequelize : sequelize(),
+      tableName: "reservation"
+    });
