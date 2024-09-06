820133ff0c47f1c1cc8db9da09a53952dbfac9b4
Jonathanbovy <jonathan.bovy@oclock.school>
2024-09-06 13:55:57 +0200

    S1D5 back - Harmonized Controller and Router Syntax

--- back/app/controllers/ticketController.js
+++ back/app/controllers/ticketController.js
@@ -1,20 +1,18 @@
 import { Ticket } from "../models/Index.js";
 
-const ticketController = {
-  async getAllTickets(req, res) {
-    const tickets = await Ticket.findAll();
-    return res.json(tickets).status(200);
-  },
+export async function getAllTickets(req, res) {
+  const tickets = await Ticket.findAll();
+  return res.json(tickets).status(200);
+}
 
-  async getOneTicket(req, res, next) {
-    const id = Number(req.params.id);
-    const ticket = await Ticket.findByPk(id);
-    if (ticket) {
-      res.json(ticket).status(200);
-    } else {
-      next();
-    }
-  },
-};
+export async function getOneTicket(req, res, next) {
+  const id = Number(req.params.id);
+  const ticket = await Ticket.findByPk(id);
+  if (ticket) {
+    res.json(ticket).status(200);
+  } else {
+    next();
+  }
+}
 
 export { ticketController };
