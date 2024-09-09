6fc0c509df2b38419575f98f34bc69b829238e1f
MaximeAverty <maxime.averty.pro@gmail.com>
2024-09-10 00:43:45 +0200

    test dynamisation

--- client/src/pages/AttractionPage.jsx
+++ client/src/pages/AttractionPage.jsx
@@ -1,82 +1,57 @@
-import React from 'react'
 
+import axios from "axios"
+import { useEffect, useState } from "react";
+import Container from "../components/Container";
+import AttractionCard from "../components/AttractionCard";
 
 function AttractionPage() {
+
+  const [attractions, setAttractions] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // await axios.get("http://localhost:3000/attractions").then(data => setAttractions(data))
+      const response = await axios.get("http://localhost:3000/attractions")
+      setAttractions(response.data)
+    }
+    fetchData()
+  },[])
+
+
    return (
     <div className="bg-backgroundColor">
       
-      <header className="text-center p-8">
-        <h1 className="text-2xl font-bold  text-white">
-          En quête de frissons et de terreur ? Découvrez des attractions toutes aussi <span className="text-red-500">terrifiantes</span> les unes que les autres...
-        </h1>
-      </header>
+        <header className="text-center p-8">
+            <h1 className="text-2xl font-bold  text-white">
+                En quête de frissons et de terreur ? Découvrez des attractions toutes aussi <span className="text-red-500">terrifiantes</span> les unes que les autres...
+            </h1>
+        </header>
       <img src="" alt="image zombie " className="w-full rounded-lg mb-4" />
       <div className="flex justify-center">
-        <img src="" alt="" className="" />
+            <img src="" alt="" className="" />
       </div>
       <section className="mt-12">
-        <h2 className="text-center text-3xl font-bold mb-8  text-white">Nos attractions</h2>
-        <div className="flex justify-center mb-6">
-          <button className="bg-red-600 text-white py-2 px-4 rounded">
-            Categories
-          </button>
-        </div>
+            <h2 className="text-center text-3xl font-bold mb-8  text-white">Nos attractions</h2>
+            <div className="flex justify-center mb-6">
+                <button className="bg-red-600 text-white py-2 px-4 rounded">
+                    Categories
+                </button>
+            </div>
 
+            <Container className="grid grid-cols-4 gap-4">
+                {
+                    attractions.map((attraction) => {
+                        return (
+                            <AttractionCard 
+                                key={attraction.id}
+                                name={attraction.name}
+                                description={attraction.description}
+                            />
+                        )
+                    })
+                }
+            </Container>
 
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-8 px-6">
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <img src="" alt="Attraction 1" className="w-full rounded-lg mb-4" />
-            <p className="text-gray-400">dqsdzqsdzq</p>
-            <button className="bg-red-600 text-white py-2 px-4 rounded mt-4">
-              + d'infos
-            </button>
-          </div>
-
-
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <img src="" alt="Attraction 2" className="w-full rounded-lg mb-4" />
-            <p className="text-gray-400">dzqssd</p>
-            <button className="bg-red-600 text-white py-2 px-4 rounded mt-4">
-              + d'infos
-            </button>
-          </div>
-
-
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <img src="" alt="Attraction 3" className="w-full rounded-lg mb-4" />
-            <p className="text-gray-400">gezrsgerdzsgrez</p>
-            <button className="bg-red-600 text-white py-2 px-4 rounded mt-4">
-              + d'infos
-            </button>
-          </div>
-
-
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <img src="" alt="Attraction 4" className="w-full rounded-lg mb-4" />
-            <p className="text-gray-400">dzqssd</p>
-            <button className="bg-red-600 text-white py-2 px-4 rounded mt-4">
-              + d'infos
-            </button>
-          </div>
-
-
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <img src="" alt="Attraction 5" className="w-full rounded-lg mb-4" />
-            <p className="text-gray-400">dzqssd</p>
-            <button className="bg-red-600 text-white py-2 px-4 rounded mt-4">
-              + d'infos
-            </button>
-          </div>
-
-
-          <div className="bg-gray-800 p-6 rounded-lg shadow-lg">
-            <img src="" alt="Attraction 6" className="w-full rounded-lg mb-4" />
-            <p className="text-gray-400">dzqssd</p>
-            <button className="bg-red-600 text-white py-2 px-4 rounded mt-4">
-              + d'infos
-            </button>
-          </div>
-        </div>
       </section>
     </div>
   );
@@ -84,3 +59,4 @@ function AttractionPage() {
 
 
 export default AttractionPage;
+
