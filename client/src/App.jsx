cc5abcabedace707252c4d1a16c57f8d2a905b9a
Jonathanbovy <jonathan.bovy@oclock.school>
2024-09-10 16:05:27 +0200

    S2D2 client - add 404 page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,70 +1,66 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import BaseLayout from "./layouts/BaseLayout.jsx"
-import HomePage from "./pages/HomePage.jsx"
-import Connexion from "./pages/Connexion.jsx"
-import Inscription from "./pages/Inscription.jsx"
-import AttractionPage from "./pages/AttractionPage.jsx"
-import Contact from "./pages/Contact.jsx"
-import UserReservation from "./pages/UserReservation.jsx"
-import AdminLayout from "./layouts/AdminLayout.jsx"
-import Overview from "./pages/admin/Overview.jsx"
-import UserProfil from "./pages/UserProfil.jsx"
-import Accordion from "./pages/FaqPage.jsx"
-import TestPage from "./pages/TestPage.jsx"
-
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import BaseLayout from "./layouts/BaseLayout.jsx";
+import HomePage from "./pages/HomePage.jsx";
+import Connexion from "./pages/Connexion.jsx";
+import Inscription from "./pages/Inscription.jsx";
+import AttractionPage from "./pages/AttractionPage.jsx";
+import Contact from "./pages/Contact.jsx";
+import UserReservation from "./pages/UserReservation.jsx";
+import AdminLayout from "./layouts/AdminLayout.jsx";
+import Overview from "./pages/admin/Overview.jsx";
+import UserProfil from "./pages/UserProfil.jsx";
+import Accordion from "./pages/FaqPage.jsx";
+import TestPage from "./pages/TestPage.jsx";
+import NotFoundPage from "./pages/404.jsx";
 
 const router = createBrowserRouter([
-  
   {
     path: "/",
     element: <BaseLayout />,
-    errorElement: <h1>OOps ! Cannot find the page !</h1>,
+    errorElement: <NotFoundPage />,
     children: [
       {
         index: true,
-        element: <HomePage />
+        element: <HomePage />,
       },
       {
         path: "attractions",
-        element: <AttractionPage/>
+        element: <AttractionPage />,
       },
       {
         path: "inscription",
-        element: <Inscription/>
+        element: <Inscription />,
       },
       {
         path: "connexion",
-        element: <Connexion/>
+        element: <Connexion />,
       },
       {
         path: "contact",
-        element: <Contact/>
+        element: <Contact />,
       },
       {
         path: "utilisateur/reservations",
-        element: <UserReservation/>
+        element: <UserReservation />,
       },
       {
         path: "utilisateur/profil",
-        element: <UserProfil/>
+        element: <UserProfil />,
       },
       {
         path: "faq",
-        element: <Accordion/>
+        element: <Accordion />,
       },
       {
         path: "test",
-        element: <TestPage />
-      }
-    ]
-  }
-])
+        element: <TestPage />,
+      },
+    ],
+  },
+]);
 
 function App() {
-  
-  return (
-    <RouterProvider router={router}/>
-  )
+  return <RouterProvider router={router} />;
 }
 
-export default App
+export default App;
