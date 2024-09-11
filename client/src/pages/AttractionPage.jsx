6f973f738506ded10095b503d66810e06a4b26fd
Jonathanbovy <jonathan.bovy@oclock.school>
2024-09-11 13:37:41 +0200

    S2D3 client - add toggleMenu in attraction page

--- client/src/pages/AttractionPage.jsx
+++ client/src/pages/AttractionPage.jsx
@@ -7,23 +7,34 @@ import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
 function AttractionPage() {
   const [attractions, setAttractions] = useState([]);
+  const [showMenu, setShowMenu] = useState(false);
+  const [category, setCategory] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       // await axios.get("http://localhost:3000/attractions").then(data => setAttractions(data))
       const response = await axios.get("http://localhost:3000/attractions");
       setAttractions(response.data);
+
+      const responseCategory = await axios.get(
+        "http://localhost:3000/categories"
+      );
+      setCategory(responseCategory.data);
     };
     fetchData();
   }, []);
+
+  const toggleMenu = () => {
+    setShowMenu(!showMenu);
+  };
 
   return (
     <div className="bg-backgroundColor">
       <header className="text-center p-8">
         <h1 className="text-2xl font-bold  text-white">
           En quête de frissons et de terreur ? Découvrez des attractions toutes
-          aussi <span className="text-primaryColor">terrifiantes</span> les unes que
-          les autres...
+          aussi <span className="text-primaryColor">terrifiantes</span> les unes
+          que les autres...
         </h1>
       </header>
       <img
@@ -38,11 +49,29 @@ function AttractionPage() {
         <h2 className="text-center text-3xl font-bold mb-8  text-white">
           Nos attractions
         </h2>
-        <div className="flex justify-center items mb-6 pb-[40px] pr-[920px]">
-          <button className="bg-primaryColor text-white py-2 px-12 rounded">
-            Categories
-            <FontAwesomeIcon icon={faChevronDown} className="ml-6" />
-          </button>
+        <div className="flex justify-center items mb-6 pb-[40px] pr-[920px] relative">
+          <div>
+            <button
+              className="bg-primaryColor text-white py-2 px-12 rounded"
+              onClick={toggleMenu}
+            >
+              Categories
+              <FontAwesomeIcon icon={faChevronDown} className="ml-6" />
+            </button>
+            {showMenu && (
+              <ul className="relative left-0 bg-[#330E14] text-white">
+                {category.map((category) => (
+                  <li
+                    key={category.id}
+                    name={category.name}
+                    className="px-4 py-2 hover:bg-primaryColor cursor-pointer"
+                  >
+                    {category.name}
+                  </li>
+                ))}
+              </ul>
+            )}
+          </div>
         </div>
 
         <Container className="grid grid-cols-3 gap-4">
@@ -50,7 +79,7 @@ function AttractionPage() {
             return (
               <AttractionCard
                 key={attraction.id}
-                id= {attraction.id}
+                id={attraction.id}
                 name={attraction.name}
                 description={attraction.description}
               />
@@ -62,7 +91,7 @@ function AttractionPage() {
           Vous avez vu 15 attractions sur 25
         </h3>
         <div className="flex justify-center mb-6">
-          <button className="bg-red-600 text-white py-4 px-20 rounded ">
+          <button className="bg-primaryColor text-white py-4 px-20 rounded ">
             VOIR TOUTES
           </button>
         </div>
