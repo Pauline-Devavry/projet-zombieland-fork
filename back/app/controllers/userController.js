ff9c23d78f7e981e8d38f353ca265ebc673f8611
pauline-devavry <pauline.devavry@oclock.school>
2024-09-11 15:22:16 +0200

    S2D3 - userController and Inscriptionpage

--- back/app/controllers/userController.js
+++ back/app/controllers/userController.js
@@ -1,15 +1,40 @@
-import bcrypt from 'bcrypt';
-import { User } from '../models/Index.js'
+const bcrypt = require('bcrypt');
+const User = require('../models/User.js');
+const saltRounds = 10;  
+
+exports.register = async (req, res) => {
+    const { name, first_name, email, password, confirmPassword } = req.body;
+
+   
+    if (password !== confirmPassword) {
+        return res.status(400).json({ error: "Les mots de passe ne correspondent pas." });
+    }
+
+    try {
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ error: "Cet utilisateur existe déjà." });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        const newUser = new User({
+            name,
+            first_name,
+            email,
+            password: hashedPassword, 
+            confirmPassword
+        });
+
+        await newUser.save();
+
+        return res.status(201).json({ message: "Utilisateur enregistré avec succès !" });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ error: "Erreur serveur, veuillez réessayer plus tard." });
+    }
+};
 
 
-export async function registerUser(req, res) {
-
-  const { password } = req.body
 
 
-  const hashedPassword = await bcrypt.hash(password, 10)
-  res.send(hashedPassword)
-
-  // stocker en db et compare 
-
-}
