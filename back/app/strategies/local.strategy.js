1797ee59aa192e8549ffa0c8f6a963dd8540df07
MaximeAverty <maxime.averty.pro@gmail.com>
2024-09-12 14:30:53 +0200

    S3D4 Back - install dependencies for the authentification and configure the local strategy

---
+++ back/app/strategies/local.strategy.js
@@ -0,0 +1,23 @@
+import { Strategy as LocalStrategy } from "passport-local"
+import bcrypt from "bcrypt"
+import { User } from "../models/Index.js"
+
+const localStrategy = new LocalStrategy({usernameField: "email"}, async (email, password, done) => {
+
+    try {
+        const user = await User.findOne({email})
+        if(!user) {
+            return done(null, false)
+        }
+        const isValidPassword = await bcrypt.compare(password, user.password)
+        if(!isValidPassword) {
+            return done(null, false)
+        }
+        return done(null, user)
+    } catch (err) {
+        return done(err)
+    }
+
+})
+
+export default localStrategy
