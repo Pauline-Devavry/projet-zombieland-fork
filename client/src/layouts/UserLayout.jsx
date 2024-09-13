23406d8172bab8e6a7caa8e57f007a60c697f13c
katiayalaoui <yalaoui.k@outlook.fr>
2024-09-13 17:19:01 +0200

    S2D5 client - AddUserLayout

--- client/src/layouts/UserLayout.jsx
+++ client/src/layouts/UserLayout.jsx
@@ -1,1 +1,43 @@
-// creer le petit menu à gauche, partie droite dynamique 
+import { NavLink, Outlet } from 'react-router-dom';
+
+function UserLayout() {
+    return (
+        <div className="flex flex-col md:flex-row bg-backgroundColor text-white">
+        
+            <div className="w-full md:w-64 p-4 border border-white">
+                <div className="flex items-center justify-between md:block">
+                    <h2>Espace personnel</h2>
+                </div>
+
+                    <nav className="mt-8">
+                        <ul>
+                        <li>
+                    <NavLink
+                        to="/utilisateur/profil"
+                        className={({ isActive }) =>
+                        isActive ? "text-primaryColor" : "text-white"}
+                    >
+                    Mon profil
+                    </NavLink>
+                        </li>
+                        <li className="mt-4">
+                    <NavLink
+                        to="/utilisateur/reservations"
+                        className={({ isActive }) =>
+                        isActive ? "text-primaryColor" : "text-white"}
+                    >
+                    Mes réservations
+                    </NavLink>
+                        </li>
+                        </ul>
+                    </nav>
+            </div>
+
+            <main className="flex-grow p-6 bg-backgroundColor w-full md:w-64 border border-white">
+                    <Outlet />
+            </main>
+        </div>
+  );
+}
+
+export default UserLayout;
