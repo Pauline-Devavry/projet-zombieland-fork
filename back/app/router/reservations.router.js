15090224174686c57dc5b706caf84411eb6d2aaa
Jonathanbovy <jonathan.bovy@oclock.school>
2024-09-16 15:12:08 +0200

    S3D1 - userReservation page changes

--- back/app/router/reservations.router.js
+++ back/app/router/reservations.router.js
@@ -1,12 +1,15 @@
+import * as reservationsController from "../controllers/reservationController.js";
+import { Router } from "express";
 
-import * as reservationsController from "../controllers/reservationController.js"
-import { Router } from "express"
+const router = Router();
 
-const router = Router()
+router.get("/reservations", reservationsController.getAllReservations);
+router.get("/reservations/:id", reservationsController.getOneReservation);
+router.get(
+  "/reservations/user/:userId",
+  reservationsController.getReservationsByUserId
+);
+router.patch("/reservations/:id", reservationsController.updateOneReservation);
+router.delete("/reservations/:id", reservationsController.deleteOneReservation);
 
-router.get("/reservations", reservationsController.getAllReservations)
-router.get("/reservations/:id", reservationsController.getOneReservation)
-router.patch("/reservations/:id", reservationsController.updateOneReservation)
-router.delete("/reservations/:id", reservationsController.deleteOneReservation)
-
-export default router
+export default router;
