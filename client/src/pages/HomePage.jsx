0bb5593e65471010e1baf2cfaea34d7f29798047
pauline-devavry <pauline.devavry@oclock.school>
2024-09-18 18:21:26 +0200

    S3D3 - add carroussel in HomePage

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -1,34 +1,39 @@
-import zombieImg from "../assets/homepage/zombie.svg"
-import zombielandCurved from "../assets/homepage/zombieland-curved.svg"
-import arrowDown from "../assets/homepage/arrow-down.svg"
-import Container from "../components/Container"
-import RedShadow from "../components/ui/RedShadow.jsx"
-import waveImg from "../assets/homepage/wave.svg"
+import zombieImg from "../assets/homepage/zombie.svg";
+import zombielandCurved from "../assets/homepage/zombieland-curved.svg";
+import arrowDown from "../assets/homepage/arrow-down.svg";
+import Container from "../components/Container";
+import RedShadow from "../components/ui/RedShadow.jsx";
+import waveImg from "../assets/homepage/wave.svg";
 
-import img1 from "../assets/homepage/image-1.png"
-import img2 from "../assets/homepage/image.png"
-import img3 from "../assets/homepage/image3.png"
-import planImg from "../assets/homepage/parc-plan-provisory.png"
+import img1 from "../assets/homepage/image-1.png";
+import img2 from "../assets/homepage/image.png";
+import img3 from "../assets/homepage/image3.png";
+import planImg from "../assets/homepage/parc-plan-provisory.png";
 
-import rollerCosterIcon from "../assets/homepage/roller-coster-icon.svg"
-import ticketIcon from "../assets/homepage/ticket-icon.svg"
-import hourglassIcon from "../assets/homepage/hourglass-icon.svg"
+import rollerCosterIcon from "../assets/homepage/roller-coster-icon.svg";
+import ticketIcon from "../assets/homepage/ticket-icon.svg";
+import hourglassIcon from "../assets/homepage/hourglass-icon.svg";
 
-import { Link } from "react-router-dom"
-import InteractiveHomeMenu from "../components/InteractiveHomeMenu.jsx"
+import { Link } from "react-router-dom";
+import InteractiveHomeMenu from "../components/InteractiveHomeMenu.jsx";
 
 import { useState, useEffect } from "react";
 import { api } from "../api/axiosConfig.js";
-import AttractionImage from "../components/AttractionImage.jsx"
+import AttractionImage from "../components/AttractionImage.jsx";
 import Marquee from "react-fast-marquee";
 
 function HomePage() {
     const [attractions, setAttractions] = useState([]);
+    const [isPaused, setIsPaused] = useState(false);
+
+    const togglePause = () => {
+        setIsPaused(!isPaused);
+    };
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await api.get("/attractions?limit=6");
+                const response = await api.get("/attractions?");
                 setAttractions(response.data);
             } catch (error) {
                 console.error("Erreur lors de la récupération des attractions:", error);
@@ -51,16 +56,29 @@ function HomePage() {
                         </h1>
                         <div className="max-w-[32.75rem] flex flex-col gap-4">
                             <p>
-                            Explorez des attractions d&apos;horreur fascinantes et plongez dans des expériences inoubliables qui éveilleront vos sens et feront battre votre cœur plus vite.
+                                Explorez des attractions d&apos;horreur fascinantes et plongez
+                                dans des expériences inoubliables qui éveilleront vos sens et
+                                feront battre votre cœur plus vite.
                             </p>
-                            <Link to={"/reserver"} className="bg-primaryColor rounded-sm px-4 py-2 w-fit">
+                            <Link
+                                to={"/reserver"}
+                                className="bg-primaryColor rounded-sm px-4 py-2 w-fit"
+                            >
                                 Faire une réservation
                             </Link>
                         </div>
                     </div>
                     <div className="flex flex-col justify-center relative md:w-1/2">
-                        <img src={zombielandCurved} alt="Zombieland" className="max-h-[58px] md:max-h-[5.5rem]" />
-                        <img src={zombieImg} alt="Image d'un zombie" className="max-h-[326px] md:max-h-[35rem]" />
+                        <img
+                            src={zombielandCurved}
+                            alt="Zombieland"
+                            className="max-h-[58px] md:max-h-[5.5rem]"
+                        />
+                        <img
+                            src={zombieImg}
+                            alt="Image d'un zombie"
+                            className="max-h-[326px] md:max-h-[35rem]"
+                        />
                         <RedShadow className="right-8" />
                     </div>
                 </div>
@@ -69,27 +87,53 @@ function HomePage() {
 
             {/* Wave section */}
             <div className="flex flex-col gap-0">
-                <img src={waveImg} alt="" className="w-full -mb-1" />
-                <div className="bg-gradient-to-b from-gradiantDarkRed to-primaryColor flex flex-col items-center gap-20 overflow-hidden py-24">
-                    <h2 className="text-heading2-mobile text-center md:text-heading2-desktop">
-                        De nombreuses attractions à découvrir
-                    </h2>
-                    <div className="mx-auto px-4 py-8">
-                        <Marquee>
-                        <div className="flex flex-wrap justify-center gap-4">
-                            {attractions.map((attraction) => (
-                                <div key={attraction.id} className="flex-basis-1/3 max-w-[calc(33%-1rem)]">
-                                    <AttractionImage
-                                        key={attraction.id}
-                                        // image_url={attraction.image_url}
-                                    />
-                                </div>
-                            ))}
-                        </div>
-                        </Marquee>
-                    </div>
-                </div>
+  <img src={waveImg} alt="" className="w-full -mb-1" />
+  <div className="bg-gradient-to-b from-gradiantDarkRed to-primaryColor flex flex-col items-center gap-20 overflow-hidden py-24">
+    <h2 className="text-heading2-mobile text-center md:text-heading2-desktop">
+      De nombreuses attractions à découvrir
+    </h2>
+    <div className="w-full">
+      <div className="mx-auto px-4 py-4" onClick={togglePause}>
+        <Marquee direction="left" speed={40} pauseOnHover={true} pauseOnClick={true} play={!isPaused} gradient={false}>
+          {attractions.slice(0, 13).map((attraction) => (
+            <div key={attraction.id} className="w-64 h-64 mx-4">
+              <AttractionImage
+                // image_url={attraction.image_url}
+              />
             </div>
+          ))}
+          {/* Répétition pour un défilement continu */}
+          {attractions.slice(0, 13).map((attraction) => (
+            <div key={`repeat-${attraction.id}`} className="w-64 h-64 mx-4">
+              <AttractionImage
+                // image_url={attraction.image_url}
+              />
+            </div>
+          ))}
+        </Marquee>
+      </div>
+      <div className="mx-auto px-4 py-4" onClick={togglePause}>
+        <Marquee direction="right" speed={40} pauseOnHover={true} pauseOnClick={true} play={!isPaused} gradient={false}>
+          {attractions.slice(13).map((attraction) => (
+            <div key={attraction.id} className="w-64 h-64 mx-4">
+              <AttractionImage
+                // image_url={attraction.image_url}
+              />
+            </div>
+          ))}
+          {/* Répétition pour un défilement continu */}
+          {attractions.slice(13).map((attraction) => (
+            <div key={`repeat-${attraction.id}`} className="w-64 h-64 mx-4">
+              <AttractionImage
+                // image_url={attraction.image_url}
+              />
+            </div>
+          ))}
+        </Marquee>
+      </div>
+    </div>
+  </div>
+</div>
 
             {/* Interactive menu seperated in its own component*/}
 
@@ -119,21 +163,17 @@ function HomePage() {
             </div>
 
             <Container className="pt-52 pb-20 flex justify-center flex-col items-center gap-12">
-                <h1 className="text-heading1-mobile md:text-heading1-desktop font-light">Carte du parc</h1>
-                <img src={planImg} className="max-h-[824px]" alt="Dessin du plan du parc d'attraction Zombieland" />
+                <h1 className="text-heading1-mobile md:text-heading1-desktop font-light">
+                    Carte du parc
+                </h1>
+                <img
+                    src={planImg}
+                    className="max-h-[824px]"
+                    alt="Dessin du plan du parc d'attraction Zombieland"
+                />
             </Container>
-
-
         </main>
-    )
-
-
-
+    );
 }
 
-export default HomePage
-
-
-
-
-
+export default HomePage;
